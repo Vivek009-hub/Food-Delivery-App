f33fd39e54db277d852e09312291f7b1ff8df369
Vivek Kumar <vivekbarwal1982@gmail.com>
2025-12-11 18:53:22 +0530

    Add optional chaining for user data access

--- frontend/src/components/OwnerOrderCard.jsx
+++ frontend/src/components/OwnerOrderCard.jsx
@@ -25,9 +25,9 @@ function OwnerOrderCard({ data }) {
     return (
         <div className='bg-white rounded-lg shadow p-4 space-y-4'>
             <div>
-                <h2 className='text-lg font-semibold text-gray-800'>{data.user.fullName}</h2>
-                <p className='text-sm text-gray-500'>{data.user.email}</p>
-                <p className='flex items-center gap-2 text-sm text-gray-600 mt-1'><MdPhone /><span>{data.user.mobile}</span></p>
+                <h2 className='text-lg font-semibold text-gray-800'>{data?.user?.fullName}</h2>
+                <p className='text-sm text-gray-500'>{data?.user?.email}</p>
+                <p className='flex items-center gap-2 text-sm text-gray-600 mt-1'><MdPhone /><span>{data?.user?.mobile}</span></p>
                 {data.paymentMethod == "online" ? <p className='gap-2 text-sm text-gray-600'>payment: {data.payment ? "true" : "false"}</p> : <p className='gap-2 text-sm text-gray-600'>Payment Method: {data.paymentMethod}</p>}
 
             </div>
